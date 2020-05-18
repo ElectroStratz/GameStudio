8fcc7d92edf22ea1e7e5c00ca65ddff2873179ce
João Freitas <joao.david.freitas@gmail.com>
2020-05-18 14:53:52 +0100

    Player Model + Animations

---
+++ Locomotion Entertainment/Assets/Camera/Camera_Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+
+    [SerializeField]
+    private Vector3 cameraOffset;
+
+    [SerializeField]
+    private float playerHeight = 2f;
+
+    private float zoom = 10f;
+
+    [SerializeField]
+    private float minZoom = 5f;
+
+    [SerializeField]
+    private float maxZoom = 15f;
+
+    [SerializeField]
+    private float zoomSpeed = 4f;
+
+    void Update()
+    {
+        zoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+    }
+    void LateUpdate()
+    {
+        transform.position = player.position - cameraOffset * zoom;
+        transform.LookAt(player.position + Vector3.up * playerHeight);
+    }
+}
