996f172f3d47d45896c6f78d984c743feb78fa4c
João Freitas <joao.david.freitas@gmail.com>
2020-05-20 19:12:51 +0100

    Walk Run Animations

---
+++ Locomotion Entertainment/Assets/Player/Player_AnimatorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player_AnimatorController : MonoBehaviour
+{
+    const float animationSmooth = .1f;
+    NavMeshAgent player;
+    Animator animator;
+    void Start()
+    {
+        player = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        float speedPercent = player.velocity.magnitude / player.speed;
+        animator.SetFloat("Speed", speedPercent, animationSmooth, Time.deltaTime);
+    }
+}
