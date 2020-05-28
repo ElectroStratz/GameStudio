a3666f0b382e05b63bb42412b34e5b0b60cf68b9
ElectroStratz <electrostratz@gmail.com>
2020-05-28 13:54:02 +0100

    Action bar Debug

--- Locomotion Entertainment/Assets/ActionBarReferenceSystem.cs
+++ Locomotion Entertainment/Assets/ActionBarReferenceSystem.cs
@@ -8,10 +8,16 @@ public class ActionBarReferenceSystem : MonoBehaviour
     public Image icon;
     PlayerInv _playerinv;
     InventoryItem item;
+
+    InventorySlotSystem[] inventorySlots;
+    public Transform actionPanel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _playerinv = GetComponent<PlayerInv>();
+        inventorySlots = actionPanel.GetComponentsInChildren<InventorySlotSystem>();
+        _playerinv.onItemChangedCallback += UpdateUI;
     }
 
     // Update is called once per frame
@@ -22,5 +28,22 @@ public class ActionBarReferenceSystem : MonoBehaviour
             var tempinvItem = _playerinv.inventory[i].GetName();
             print(tempinvItem);
         }
+    }
+
+    void UpdateUI()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (_playerinv.inventory[i].GetAmount() > 0)
+            {
+                inventorySlots[i].AddItem(_playerinv.inventory[i]);
+            }
+            else
+            {
+                inventorySlots[i].ClearSlot();
+            }
+
+        }
+        print(inventorySlots.ToString());
     }
 }
